5930e9f6f6606917b49528650040cde99e5b790a
Stoyan <slownessbg@gmail.com>
2020-10-02 23:16:50 +0300

    adding excel library

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿namespace ExcelDuplicateFinder
+﻿using System;
+using System.Linq;
+using LinqToExcel;
+
+namespace ExcelDuplicateFinder
 {
     class Program
     {
         static void Main(string[] args)
         {
-            const string excelFilePath = "./data.xls";
+            const string excelFilePath = "./data.xlsx";
 
-            Console.WriteLine(excelFilePath + " is loaded!");
+            var excel = new ExcelQueryFactory(excelFilePath);
+
+            var cols = excel.WorksheetNoHeader();
+
+            var rows = cols.Select(x => x[0]).ToArray();
+            Console.WriteLine(rows[1]);
         }
     }
 }
