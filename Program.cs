ac621972e5dd7960262fa28c1b05b0c9caf7268a
Stoyan <slownessbg@gmail.com>
2020-10-03 22:47:32 +0300

    adding the basic functionality

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using LinqToExcel;
 
@@ -15,7 +16,30 @@ namespace ExcelDuplicateFinder
             var cols = excel.WorksheetNoHeader();
 
             var rows = cols.Select(x => x[0]).ToArray();
-            Console.WriteLine(rows[1]);
+
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var cell in rows)
+            {
+                string text = cell.Cast<string>();
+
+                if (!dictionary.ContainsKey(text))
+                {
+                    dictionary.Add(text, 1);
+                }
+                else
+                {
+                    dictionary[text]++;
+                }
+            }
+
+            foreach (var pair in dictionary)
+            {
+                if (pair.Value > 1)
+                {
+                    Console.WriteLine($"\"{pair.Key}\" was found {pair.Value} times");
+                }
+            }
         }
     }
 }
