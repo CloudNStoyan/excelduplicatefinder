2ea77d9bb5ce171b3ea9eab11cc53588cb05f8f7
Stoyan <slownessbg@gmail.com>
2020-10-04 15:09:22 +0300

    adding quality of life changes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using LinqToExcel;
 
@@ -9,37 +10,53 @@ namespace ExcelDuplicateFinder
     {
         static void Main(string[] args)
         {
-            const string excelFilePath = "./data.xlsx";
-
-            var excel = new ExcelQueryFactory(excelFilePath);
-
-            var cols = excel.WorksheetNoHeader();
-
-            var rows = cols.Select(x => x[0]).ToArray();
-
-            var dictionary = new Dictionary<string, int>();
-
-            foreach (var cell in rows)
+            if (args != null && args.Length > 0)
             {
-                string text = cell.Cast<string>();
+                string filePath = args[0];
 
-                if (!dictionary.ContainsKey(text))
+                if (File.Exists(filePath))
                 {
-                    dictionary.Add(text, 1);
+                    var excel = new ExcelQueryFactory(filePath);
+
+                    var cols = excel.WorksheetNoHeader();
+
+                    var rows = cols.Select(x => x[0]).ToArray();
+
+                    var dictionary = new Dictionary<string, int>();
+
+                    foreach (var cell in rows)
+                    {
+                        string text = cell.Cast<string>();
+
+                        if (!dictionary.ContainsKey(text))
+                        {
+                            dictionary.Add(text, 1);
+                        }
+                        else
+                        {
+                            dictionary[text]++;
+                        }
+                    }
+
+                    foreach (var pair in dictionary)
+                    {
+                        if (pair.Value > 1)
+                        {
+                            Console.WriteLine($"\"{pair.Key}\" was found {pair.Value} times");
+                        }
+                    }
                 }
                 else
                 {
-                    dictionary[text]++;
+                    Console.WriteLine("The path is invalid!");
                 }
+            }
+            else
+            {
+                Console.WriteLine("You need to pass the filepath as first argument to the program!");
             }
 
-            foreach (var pair in dictionary)
-            {
-                if (pair.Value > 1)
-                {
-                    Console.WriteLine($"\"{pair.Key}\" was found {pair.Value} times");
-                }
-            }
+            Console.ReadKey();
         }
     }
 }
